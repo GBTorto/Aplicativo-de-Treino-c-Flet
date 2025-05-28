a5bc0a7b8f435f0e94a87246f47904c185de1d1b
DEV-T-2025 <moraisgabriel867@gmail.com>
2025-05-28 16:35:07 -0300

    começo dos labels no arquivo info_exercicios.py, estou pensando em como adicionar um gráfico para as informações dos labels

--- src/info_exercicios.py
+++ src/info_exercicios.py
@@ -11,7 +11,29 @@ def main(page: ft.Page, id_exercicio):
     repeticoes_banco = info['repeticoes']
     peso_banco = info['peso']
 
-    
+    data = ft.TextField(label="Data")
+    series = ft.TextField(label="Séries", disabled=True)
+    repeticoes = ft.TextField(label="Repetições", disabled=True)
+    peso = ft.TextField(label="Peso", disabled=True)
+
+    def on_data_change(e):
+        series.disabled = not bool(data.value)
+        repeticoes.disabled = True
+        peso.disabled = True
+        page.update()
+
+    def on_series_change(e):
+        repeticoes.disabled = not bool(series.value)
+        peso.disabled = True
+        page.update()
+
+    def on_repeticoes_change(e):
+        peso.disabled = not bool(repeticoes.value)
+        page.update()
+
+    data.on_change = on_data_change
+    series.on_change = on_series_change
+    repeticoes.on_change = on_repeticoes_change
 
     page.views.append(
         ft.View(
@@ -19,6 +41,10 @@ def main(page: ft.Page, id_exercicio):
             controls=[
                 ft.Text(nome_exercicio),
                 ft.Text(f"Teste - ID do exercício: {id_exercicio}"),
+                data,
+                series,
+                repeticoes,
+                peso,
                 ft.Button(text="Voltar",
                         on_click=lambda e: page.go("/tela_principal"))
             ]
